3124d54a36f751934b38ec80daabf5bcf252a093
kmanadkat <kmanadkat@gmail.com>
2025-11-29 12:12:14 +0530

    feat: Prettier Setup

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,22 +1,22 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import { Button } from './components/ui/button'
+import { useState, useEffect } from 'react';
+import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState<string>("")
+   const [message, setMessage] = useState<string>('');
 
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((response) => response.json())
-      .then((data) => setMessage(data.message))
-  }, [])
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((response) => response.json())
+         .then((data) => setMessage(data.message));
+   }, []);
 
-  return (
-    <div className='p-4'>
-      <h1 className='font-bold text-2xl text-purple-400'>{message}</h1>
-      <Button className='my-4'>Click me</Button>
-    </div>
-  )
+   return (
+      <div className="p-4">
+         <h1 className="font-bold text-2xl text-purple-400">{message}</h1>
+         <Button className="my-4">Click me</Button>
+      </div>
+   );
 }
 
-export default App
+export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,20 +1,20 @@
-import express, { type Request, type Response } from "express";
+import express, { type Request, type Response } from 'express';
 
 // Load environment variables from .env file
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-    res.json({ message: "Hello World 🌍!" });
+app.get('/', (req: Request, res: Response) => {
+   res.json({ message: 'Hello World 🌍!' });
 });
 
-app.get("/api/hello", (req: Request, res: Response) => {
-    res.json({ message: "Hello World 🌍!" });
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.json({ message: 'Hello World 🌍!' });
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server started on port http://localhost:${port}`);
+   console.log(`Server started on port http://localhost:${port}`);
 });
