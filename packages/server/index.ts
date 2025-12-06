3bb08c424b44da80b0dea9467ecaac59806033d2
kmanadkat <kmanadkat@gmail.com>
2025-12-06 13:36:02 +0530

    feat: Basic Chat Conversations

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,17 +1,35 @@
 import express, { type Request, type Response } from 'express';
-
-// Load environment variables from .env file
 import dotenv from 'dotenv';
+import OpenAI from 'openai';
+
 dotenv.config();
+const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-   res.json({ message: 'Hello World 🌍!' });
+   res.json({ message: 'API Running' });
 });
 
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.json({ message: 'Hello World 🌍!' });
+const conversations = new Map<string, string>();
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+   const { prompt, conversationId } = req.body;
+   console.log('Before: ', conversations);
+
+   const response = await client.responses.create({
+      model: 'gpt-4.1-nano-2025-04-14',
+      input: prompt,
+      temperature: 0.2,
+      max_output_tokens: 100,
+      previous_response_id: conversations.get(conversationId),
+   });
+   conversations.set(conversationId, response.id);
+   console.log('After: ', conversations);
+
+   res.json({ message: response.output_text });
+   return;
 });
 
 const port = process.env.PORT || 3000;
